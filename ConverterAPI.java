e846ce9d305921654a732b4211a2edd2d1c48e1b
sardor <alimukhammad1@gmail.com>
2023-04-12 23:30:38 -0400

    update

--- ConverterAPI.java
+++ ConverterAPI.java
@@ -1,3 +1,5 @@
+import java.net.http.HttpResponse;
+
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClients;
@@ -5,7 +7,7 @@ import org.apache.http.util.EntityUtils;
 
 public class ConverterAPI {
     private static final String API_ENDPOINT = "https://www.amdoren.com";
-    private static final String API_KEY = "7pxNbvXgH6XYcP6NRLAR8bNGndieh2";
+    private static final String API_KEY = "";
     
     public static void main(String[] args) throws Exception {
         HttpClient client = HttpClients.createDefault();
