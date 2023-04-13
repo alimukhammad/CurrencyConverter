7dfd5a4fb7f1eac0207426bf019bfede3b925dc7
sardor <alimukhammad1@gmail.com>
2023-04-12 23:30:00 -0400

    first commit

---
+++ ConverterAPI.java
@@ -0,0 +1,19 @@
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class ConverterAPI {
+    private static final String API_ENDPOINT = "https://www.amdoren.com";
+    private static final String API_KEY = "7pxNbvXgH6XYcP6NRLAR8bNGndieh2";
+    
+    public static void main(String[] args) throws Exception {
+        HttpClient client = HttpClients.createDefault();
+        HttpGet request = new HttpGet(API_ENDPOINT);
+        request.addHeader("Authorization", "Bearer " + API_KEY);
+        HttpResponse response = client.execute(request);
+        String json = EntityUtils.toString(response.getEntity());
+        // Parse the JSON response into a Java object using a JSON parsing library
+        // ...
+    }
+}
